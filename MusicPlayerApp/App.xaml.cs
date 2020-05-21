eeb09260037591c0d4166da0052c2f5c578ccef2
crimer <99nik.pro@gmail.com>
2020-05-21 12:03:03 +1000

    Add libraries and ViewModels

--- MusicPlayerApp/App.xaml.cs
+++ MusicPlayerApp/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MusicPlayerApp.View;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace MusicPlayerApp
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new LandingPage());
         }
 
         protected override void OnStart()
